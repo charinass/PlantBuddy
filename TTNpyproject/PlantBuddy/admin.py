7a2011d200d5fd897d723da1f7359da9970ad01c
charinass <cha_sor@yahoo.co.uk>
2021-10-15 16:25:45 +0200

    registered the models' tables so it appears on admin page

--- TTNpyproject/PlantBuddy/admin.py
+++ TTNpyproject/PlantBuddy/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from PlantBuddy.models import DeviceModel, ServiceModel, GatewayModel, ConnectionModel
+
 
 # Register your models here.
+admin.site.register(DeviceModel)
+admin.site.register(ServiceModel)
+admin.site.register(GatewayModel)
+admin.site.register(ConnectionModel)
