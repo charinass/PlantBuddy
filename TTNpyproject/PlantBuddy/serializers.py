d4c9d71c93ca13ffd0edf79bdd48d3b1a3ec0205
charinass <cha_sor@yahoo.co.uk>
2021-10-15 16:15:43 +0200

    added more classes to serializers.py

--- TTNpyproject/PlantBuddy/serializers.py
+++ TTNpyproject/PlantBuddy/serializers.py
@@ -29,3 +29,27 @@ class ServiceSerializer(serializers.Serializer):
 
     def create(self, validated_data):
         return ServiceModel.objects.create(**validated_data)
+
+
+class GatewaySerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    gtw_network_id = serializers.CharField()
+    latitude = serializers.IntegerField()
+    longitude = serializers.IntegerField()
+    altitude = serializers.IntegerField()
+    location = serializers.CharField()
+
+    def create(self, validated_data):
+        return GatewayModel.objects.create(**validated_data)
+
+
+class ConnectionSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    gateway_id = serializers.IntegerField(read_only=True)
+    service_id = serializers.IntegerField(read_only=True)
+    device_id = serializers.IntegerField(read_only=True)
+    rssi = serializers.IntegerField()
+    snr = serializers.IntegerField()
+
+    def create(self, validated_data):
+        return ConnectionModel.objects.create(**validated_data)
